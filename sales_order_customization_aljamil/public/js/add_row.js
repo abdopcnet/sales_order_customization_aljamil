372947454217262aded6a37f32699ff642b5b12d
abdopcnet <abdopcnet@gmail.com>
2025-12-16 06:08:46 +0300

    🔄 add_row.js

--- sales_order_customization_aljamil/public/js/add_row.js
+++ sales_order_customization_aljamil/public/js/add_row.js
@@ -1,10 +1,10 @@
 frappe.ui.form.on('Sales Order', {
     validate: function(frm) {
-        // تحقق من جدول التأمين
+        // Check insurance table
         if (frm.doc.custom__insurance_data && frm.doc.custom__insurance_data.length > 1) {
             frappe.throw(__('لا يمكن إضافة أكثر من صف واحد في جدول التأمين.'));
         }
-        // تحقق من جدول المقاسات
+        // Check sizes table
         if (frm.doc.custom_size && frm.doc.custom_size.length > 1) {
             frappe.throw(__('لا يمكن إضافة أكثر من صف واحد في جدول المقاسات.'));
         }
@@ -12,7 +12,7 @@ frappe.ui.form.on('Sales Order', {
 
     custom__insurance_data_add: function(frm) {
         if (frm.doc.custom__insurance_data.length > 1) {
-            // حذف الصف الزائد فوراً
+            // Delete extra row immediately
             frm.doc.custom__insurance_data.splice(1);
             frm.refresh_field('custom__insurance_data');
             frappe.msgprint(__('مسموح فقط بسطر واحد في جدول التأمين.'));
@@ -21,7 +21,7 @@ frappe.ui.form.on('Sales Order', {
 
     custom_size_add: function(frm) {
         if (frm.doc.custom_size.length > 1) {
-            // حذف الصف الزائد فوراً
+            // Delete extra row immediately
             frm.doc.custom_size.splice(1);
             frm.refresh_field('custom_size');
             frappe.msgprint(__('مسموح فقط بسطر واحد في جدول المقاسات.'));
