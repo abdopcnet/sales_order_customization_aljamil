095e2dd2699ea20095574a81165c86d8eae17b9b
abdopcnet <abdopcnet@gmail.com>
2025-12-16 06:08:46 +0300

    🔄 approval_amount_limit.js

--- sales_order_customization_aljamil/public/js/approval_amount_limit.js
+++ sales_order_customization_aljamil/public/js/approval_amount_limit.js
@@ -2,12 +2,12 @@ frappe.ui.form.on('Sales Order', {
     validate: function(frm) {
         let total = 0;
 
-        // جمع قيم custom_approval_amount في جدول البنود
+        // Sum custom_approval_amount values in items table
         frm.doc.items.forEach(function(row) {
             total += row.custom_approval_amount || 0;
         });
 
-        // مقارنة الإجمالي مع الحقل الرئيسي
+        // Compare total with main field
         if (total > (frm.doc.custom_approval_amount || 0)) {
             frappe.throw(__('إجمالي مبلغ الموافقة في البنود يتجاوز المبلغ المحدد في أمر البيع.'));
         }
