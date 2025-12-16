0809c27e1ec0555a83f915a737f556a1485e29ca
abdopcnet <abdopcnet@gmail.com>
2025-12-16 05:42:01 +0300

    🔄 total_deduction.js

--- sales_order_customization_aljamil/public/js/total_deduction.js
+++ sales_order_customization_aljamil/public/js/total_deduction.js
@@ -1,31 +1,34 @@
 // تحديث إجمالي الخصم في Sales Order
 
 frappe.ui.form.on('Sales Order Item', {
-    discount_amount: function(frm, cdt, cdn) {
-        update_total_deduction(frm);
-    },
-    qty: function(frm, cdt, cdn) {
-        update_total_deduction(frm);
-    }
+	discount_amount: function (frm, cdt, cdn) {
+		update_total_deduction(frm);
+	},
+	qty: function (frm, cdt, cdn) {
+		update_total_deduction(frm);
+	},
 });
 
 frappe.ui.form.on('Sales Order', {
-    onload: function(frm) {
-        update_total_deduction(frm); // تحديث عند تحميل الفورم
-    },
-    refresh: function(frm) {
-        update_total_deduction(frm); // تحديث عند التحديث
-    }
+	onload: function (frm) {
+		update_total_deduction(frm);
+	},
+	refresh: function (frm) {
+		update_total_deduction(frm);
+	},
 });
 
 function update_total_deduction(frm) {
-    let total_deduction = 0;
+	let total_deduction = 0;
 
-    (frm.doc.items || []).forEach(item => {
-        let discount = (item.discount_amount || 0) * (item.qty || 0);
-        total_deduction += discount;
-    });
+	(frm.doc.items || []).forEach((item) => {
+		let discount = (item.discount_amount || 0) * (item.qty || 0);
+		total_deduction += discount;
+	});
 
-    // تحديث القيمة داخل الفورم
-    frm.set_value('custom_total_deduction', total_deduction);
+	// Only update if value has changed to prevent marking document as dirty
+	let current_value = flt(frm.doc.custom_total_deduction || 0);
+	if (Math.abs(current_value - total_deduction) > 0.01) {
+		frm.set_value('custom_total_deduction', total_deduction);
+	}
 }
