c7d83977339e7c87e37e35eefbe585954e97f375
abdopcnet <abdopcnet@gmail.com>
2025-12-23 02:40:34 +0300

    ❌ items_available.js

--- sales_order_customization_aljamil/public/js/items_available.js
+++
@@ -1,9 +0,0 @@
-// Prevent submit only if quantities are not fully available
-frappe.ui.form.on('Sales Order', {
-    before_submit(frm) {
-        if (frm.doc.custom_items_available !== 100) {
-            frappe.throw(__('⚠️ لا يمكن تسجيل أمر البيع لأن بعض الأصناف غير متوفرة بالكامل في المخزون.'));
-        }
-    }
-});
-
